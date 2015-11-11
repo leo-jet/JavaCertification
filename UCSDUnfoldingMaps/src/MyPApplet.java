c79ee14c875808f9efb449f085b13e9c85e2054a
leo-jet <emmanuel.tematio@gmail.com>
2015-11-11 11:20:39 +0100

    Creation du depot pour le projet du cours 1 de la certification. Actually I'm in week 2.

---
+++ UCSDUnfoldingMaps/src/MyPApplet.java
@@ -0,0 +1,15 @@
+import processing.core.*;
+
+
+public class MyPApplet extends PApplet {
+
+	//function executed once
+	public void setup(){
+		
+	}
+	
+	//function looped often
+	public void draw(){
+		
+	}
+}
