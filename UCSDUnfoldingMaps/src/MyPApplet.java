bcb370e271f71101f992886527ad90a616cee5a9
leo-jet <emmanuel.tematio@gmail.com>
2015-11-11 12:21:32 +0100

    Ends of my papplet test. In my papplet, the color of articial sun changes according to the time (if hour <12 or not)

--- UCSDUnfoldingMaps/src/MyPApplet.java
+++ UCSDUnfoldingMaps/src/MyPApplet.java
@@ -8,6 +8,7 @@ public class MyPApplet extends PApplet {
 
 	private String URL = "palmTrees.jpg";
 	private PImage backgroundImg;
+	private float hour;
 	//function executed once
 	public void setup(){
 		size(200, 200);
@@ -20,7 +21,13 @@ public class MyPApplet extends PApplet {
 		//height is a PApplet variable. it tells that the image height should be the same with the canvas
 		backgroundImg.resize(0,height);
 		image(backgroundImg, 0, 0);
-		fill(255,250,0);
+		hour = hour();
+		if(hour<12){
+			fill(255,250,0);
+		}else{
+			fill(25,150,200);
+		}
+		text("heure"+hour, 100, 150);
 		ellipse(width/4, height/5, width/5, height/5);
 		
 	}
